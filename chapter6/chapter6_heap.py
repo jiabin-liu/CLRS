6375563a9f11f695d37abbc07e9fabbf5d82e1f8
swuerth <swuerth@berkeley.edu>
2017-03-07 10:20:23 -0800

    put ch6 stuff in new directory

---
+++ chapter6/chapter6_heap.py
@@ -0,0 +1,63 @@
+def tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the label.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def build_binary_ones(h):
+    """
+    >>> print_tree(build_binary_ones(1))
+    1
+      1
+      1
+    >>> print_tree(build_binary_ones(2))
+    1
+      1
+        1
+        1
+      1
+        1
+        1
+    """
+    if h==0:
+        return tree(1)
+    return tree(1,[build_binary_ones(h-1),build_binary_ones(h-1)])
